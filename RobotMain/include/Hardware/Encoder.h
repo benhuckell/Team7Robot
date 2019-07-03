f1729190b461a5b7a8029ced4b50a649cd662414
Josh Zindler <joshzindler1498@gmail.com>
2019-07-02 18:15:07 -0700

    change 1

---
+++ RobotMain/include/Hardware/Encoder.h
@@ -0,0 +1,9 @@
+ namespace HWNameSpace {
+    class Encoder {
+        public:
+            Encoder();
+            int getSpeed(); 
+            int getCount();
+            void resetCount(); //Be Careful!!!
+    };
+ }
