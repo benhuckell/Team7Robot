585f7f6fbfdfed4a5180736923ee9cd4e1c6977a
Ben <b.huckell@hotmail.com>
2019-07-09 13:31:59 -0700

    WORKING with changes, up to timers, OLED functional

--- RobotMain/include/Hardware/Encoder.h
+++ RobotMain/include/Hardware/Encoder.h
@@ -1,13 +1,11 @@
 #pragma once
 #include "Arduino.h"
-//#include <chrono>
-//#include <ctime>
-//#include "sys/time.h"
 
 class Encoder {
     public:
         Encoder(PinName encoder_port_1, PinName encoder_port_2);
-        void update();
+        void update_port_1();
+        void update_port_2();
         int getSpeed(); 
         int getCount();
         void resetCount(); //Be Careful!!!
@@ -15,11 +13,14 @@ class Encoder {
     private:
         PinName encoder_port_1;
         PinName encoder_port_2;
+        unsigned int timeSinceLastPulse_1;
+        unsigned int timeOfLastPulse_1;
+        unsigned int timeSinceLastPulse_2;
+        unsigned int timeOfLastPulse_2;
         int speed;
         int rpm;
         int count;
-        float timeSinceLastPulse;
-        float timeOfLastPulse;
+        int direction;
         const float WheelDiameter = 0.02; //metres
         const int ticksPerRotation = 90;
 
--- RobotMain/src/Hardware/Encoder.cpp
+++ RobotMain/src/Hardware/Encoder.cpp
@@ -6,15 +6,33 @@ Encoder::Encoder(PinName encoder_port_1, PinName encoder_port_2) {
 }
 
 //gets called on every rising edge of the encoder output
-void Encoder::update() {
-    /* 
-    timeSinceLastPulse = timeOfLastPulse - std::clock();
-    timeOfLastPulse = std::clock();
+void Encoder::update_port_1() {
+    if(millis() - timeOfLastPulse_2 < timeOfLastPulse_2 - timeOfLastPulse_1){ //if encoder 'slot' is the leading one
+        direction = 1;
+    }
+    timeSinceLastPulse_1 = timeOfLastPulse_1 - millis();
+    timeOfLastPulse_1 = millis();
+    
     count++;
-    float ticksPerSecond = 1/timeSinceLastPulse; 
+    float ticksPerSecond = 1/timeSinceLastPulse_1; 
     float rotationsPerSecond = ticksPerSecond/ticksPerRotation;
     rpm = rotationsPerSecond*60;
-    speed = rotationsPerSecond*PI*WheelDiameter;*/
+    speed = rotationsPerSecond*PI*WheelDiameter;direction;
+}
+
+void Encoder::update_port_2() {
+    if(millis() - timeOfLastPulse_2 > timeOfLastPulse_2 - timeOfLastPulse_1){ //if encoder 'slot' is the trailing one
+        direction = 1;
+    }
+    timeSinceLastPulse_2 = timeOfLastPulse_2 - millis();
+    timeOfLastPulse_2 = millis();
+
+    direction = 1;
+    count++;
+    float ticksPerSecond = 1/timeSinceLastPulse_2; 
+    float rotationsPerSecond = ticksPerSecond/ticksPerRotation;
+    rpm = rotationsPerSecond*60;
+    speed = rotationsPerSecond*PI*WheelDiameter*direction;
 }
 
 int Encoder::getSpeed() {
